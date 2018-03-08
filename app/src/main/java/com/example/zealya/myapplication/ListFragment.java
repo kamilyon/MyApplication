0547f2d13c2627de2eab8bd68f48f86e4ac150d5
Damien Gilliams <uffoward@gmail.com>
2018-03-08 14:07:45 -0500

    Create ListFragment.java

---
+++ app/src/main/java/com/example/zealya/myapplication/ListFragment.java
@@ -0,0 +1,16 @@
+//todo: create XML file for your fragment
+
+public class ListFragment extends Fragment {
+//listfragment variables
+
+    @Override
+    public void onCreate(...) {
+
+    }
+    
+    @Override
+    public void onCreateView(...) {
+        //todo follow the fragment documentation to inflate your fragment that you created in XML
+    }
+
+}
